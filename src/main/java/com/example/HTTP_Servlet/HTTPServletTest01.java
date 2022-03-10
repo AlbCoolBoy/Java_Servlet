a15b36d15d965e930ffb50cfd9f1123fbbff1da4
ALB <9752591+shao-mingtao@user.noreply.gitee.com>
2022-03-10 17:06:14 +0800

    HttpServlet

---
+++ src/main/java/com/example/HTTP_Servlet/HTTPServletTest01.java
@@ -0,0 +1,7 @@
+package com.example.HTTP_Servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class HTTPServletTest01 extends HttpServlet {
+
+}
---
+++ src/main/java/com/example/HTTP_Servlet/LoginServlet/LoginServlet.java
@@ -0,0 +1,18 @@
+package com.example.HTTP_Servlet.LoginServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>login successfully</h1>");
+    }
+}
