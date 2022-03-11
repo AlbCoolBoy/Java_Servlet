52dfb61f3518ab02d5c2bc7192b5708402af8c63
ALB <9752591+shao-mingtao@user.noreply.gitee.com>
2022-03-11 14:57:01 +0800

    HttpServlet

---
+++ src/main/java/com/example/HTTP_Servlet/HelloPage/LoginAsWelcomePage.java
@@ -0,0 +1,18 @@
+package com.example.HTTP_Servlet.HelloPage;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginAsWelcomePage extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        
+        
+    }
+}
---
+++ src/main/java/com/example/HTTP_Servlet/HttpServletRequest/ResquestTest.java
@@ -0,0 +1,18 @@
+package com.example.HTTP_Servlet.HttpServletRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ResquestTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println(req.toString());
+    }
+}
